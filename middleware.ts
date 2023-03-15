fe8402b373414037cf9a194b06b0c9ff35f33887
Kayode Ezike <kezike13@gmail.com>
2023-03-15 17:12:01 -0400

    switches req.statusListManager to req.statusManager for consistency with verifyStatusRepoAccess; switches from hasStatusRepoAccess to hasStatusAuthority; adds style updates

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { getStatusListManager } from './status';
 // retrieves status list manager
 export async function getCredentialStatusListManager(req, res, next) {
     try {
-      req.statusListManager = await getStatusListManager();
+      req.statusManager = await getStatusListManager();
       next();
     } catch (error) {
       return res.send('Failed to retrieve credential status list manager');
@@ -12,27 +12,27 @@ export async function getCredentialStatusListManager(req, res, next) {
 
 // extracts access token from request header
 function extractAccessToken(headers) {
-  if (!headers.authorization) {
-    return;
-  }
-  const [scheme, token] = headers.authorization.split(' ');
-  if (scheme === 'Bearer') {
-    return token;
-  }
+    if (!headers.authorization) {
+        return;
+    }
+    const [scheme, token] = headers.authorization.split(' ');
+    if (scheme === 'Bearer') {
+        return token;
+    }
 }
 
 // verifies whether issuer client has access to status repo
-export const verifyStatusRepoAccess = async (req, res, next) => {
+export async function verifyStatusRepoAccess(req, res, next) {
     const { headers } = req;
-    // Verify that access token was included in request
+    // verify that access token was included in request
     const accessToken = extractAccessToken(headers);
     if (!accessToken) {
       return res.send('Failed to provide access token in request');
     }
-    // Check if issuer client has access to status repo
-    const hasAccess = await req.credStatusClient.hasStatusRepoAccess(accessToken);
+    // check if issuer client has access to status repo
+    const hasAccess = await req.statusManager.hasStatusAuthority(accessToken);
     if (!hasAccess) {
       return res.send('Issuer is unauthorized to access status repo');
     }
     next();
-};
+}
