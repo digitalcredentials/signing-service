27c664f23b26e48e72e0c26d2fb9ef20646ca6fd
James Chartrand <jc.chartrand@gmail.com>
2025-07-25 12:11:54 -0400

    rework did generation to use did-method-web lib rather than interop

--- src/generate.js
+++ src/generate.js
@@ -1,31 +1,25 @@
 import { generateSecretKeySeed } from 'bnid'
 import decodeSeed from './utils/decodeSeed.js'
 import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020'
-import { CryptoLD } from 'crypto-ld'
 
 import { driver as keyDriver } from '@digitalbazaar/did-method-key'
-import { driver as webDriver } from '@interop/did-web-resolver'
-const cryptoLd = new CryptoLD()
-cryptoLd.use(Ed25519VerificationKey2020)
-const didDriver = webDriver({ cryptoLd })
+import { driver as webDriver } from '@digitalbazaar/did-method-web'
 
 export default async function generateSeed({ url = false }) {
   const encodedSeed = await generateSecretKeySeed()
   const seed = await decodeSeed(encodedSeed)
-  let didDocument
-  if (url) {
-    ;({ didDocument } = await didDriver.generate({ seed, url }))
-  } else {
-    const didKeyDriver = keyDriver()
-    didKeyDriver.use({
-      multibaseMultikeyHeader: 'z6Mk',
-      fromMultibase: Ed25519VerificationKey2020.from
-    })
-    const verificationKeyPair = await Ed25519VerificationKey2020.generate({
-      seed
-    })
-    ;({ didDocument } = await didKeyDriver.fromKeyPair({ verificationKeyPair }))
-  }
+  const didDriver = url ? webDriver() : keyDriver()
+  didDriver.use({
+    multibaseMultikeyHeader: 'z6Mk',
+    fromMultibase: Ed25519VerificationKey2020.from
+  })
+  const verificationKeyPair = await Ed25519VerificationKey2020.generate({
+    seed
+  })
+  const { didDocument } = await didDriver.fromKeyPair({
+    verificationKeyPair,
+    ...(url && { url })
+  })
   const did = didDocument.id
   return { seed: encodedSeed, decodedSeed: seed, did, didDocument }
 }
